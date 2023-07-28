5053bf954341a628671a83e8853ebd2d8fadf059
Aashiq-J <aashiq.jacob@ibm.com>
2023-07-28 18:35:28 +0530

    feat: add output for zone and rule ids

--- profiles/fscloud/outputs.tf
+++ profiles/fscloud/outputs.tf
@@ -2,25 +2,34 @@
 # # Outputs
 # ##############################################################################
 
-# output "zone_ids" {
-#   value       = [module.cbr_rule_multi_service_profile_1[*].zone_ids, module.cbr_rule_multi_service_profile_2[*].zone_ids]
-#   description = "CBR zone resource instance id(s)"
-# }
+output "cbr_zone_ids" {
+  value = { for key, value in local.cbr_zones :
+    key => {
+      zone_id = value.zone_id
+  } }
+  description = "CBR zone resource instance id(s)"
+}
 
-# output "zone_crns" {
-#   value       = [module.cbr_rule_multi_service_profile_1[*].zone_crns, module.cbr_rule_multi_service_profile_2[*].zone_crns]
-#   description = "CBR zones crn(s)"
-# }
+output "cbr_zone_vpcs_id" {
+  value = {
+    # key => {
+    zone_id = local.cbr_zone_vpcs.zone_id
+  }
+  description = "CBR zone resource instance id(s)"
+}
 
 # output "zone_hrefs" {
 #   value       = [module.cbr_rule_multi_service_profile_1[*].zone_hrefs, module.cbr_rule_multi_service_profile_2[*].zone_hrefs]
 #   description = "CBR zones href(s)"
 # }
 
-# output "rule_ids" {
-#   value       = [module.cbr_rule_multi_service_profile_1[*].rule_ids, module.cbr_rule_multi_service_profile_2[*].rule_ids]
-#   description = "CBR rule id(s)"
-# }
+output "rule_ids" {
+  value = { for key, value in module.cbr_rule :
+    key => {
+      rule_id = value.rule_id
+  } }
+  description = "CBR rule id(s)"
+}
 
 # output "rule_crns" {
 #   value       = [module.cbr_rule_multi_service_profile_1[*].rule_crns, module.cbr_rule_multi_service_profile_2[*].rule_crns]
@@ -47,12 +56,12 @@
 
 # output "ip_zone_list" {
 #   value = local.allow_rules_by_service
+# # }
+
+# output "somevalue" {
+#   value = local.cbr_zones
 # }
 
-output "somevalue" {
-  value = local.cbr_zone_ip
-}
-
-# output "prewired_rule_contexts_by_service" {
-#   value = local.prewired_rule_contexts_by_service
-# }
+# # output "prewired_rule_contexts_by_service" {
+# #   value = local.prewired_rule_contexts_by_service
+# # }
