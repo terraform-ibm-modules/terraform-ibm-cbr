a0ec973faf2f2d00e8f9bed2cb082503e1463357
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-06-04 10:16:01 +0100

    SKIP UPGRADE TEST

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -19,7 +19,7 @@ module "key_protect_module" {
   key_protect_name  = "${var.prefix}-key-protect-instance"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
-  service_endpoints = "private"
+  allowed_network   = "private-only"
   plan              = "tiered-pricing"
 }
 
