65fca6d363f64434744b1ec3c6127b4a0077c1bf
Akash Kumar <akash.kumar@ibm.com>
2024-07-25 21:45:01 +0530

    minor changes

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -87,7 +87,7 @@ module "cbr_account_level" {
     }
     "mqcloud" : {
       "enforcement_mode" = "disabled"
-      "region"           = "eu-fr2" 
+      "region"           = "eu-fr2" # BNPP region (eu-fr2)
     }
   }
 
