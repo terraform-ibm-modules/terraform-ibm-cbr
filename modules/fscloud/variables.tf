601b087a70cb69c856433337cc26af7e523ee2cc
Rajat Agrawal <rajatatibm@gmail.com>
2024-09-04 11:09:36 +0100

    feat: add missing geography field

--- modules/fscloud/variables.tf
+++ modules/fscloud/variables.tf
@@ -299,14 +299,22 @@ variable "target_service_details" {
   }
   validation {
     condition = alltrue([
+      for target_service_name, attributes in var.target_service_details :
+      contains([
+      "cloud-object-storage", "codeengine", "containers-kubernetes", "containers-kubernetes-cluster", "containers-kubernetes-management", "databases-for-cassandra", "databases-for-elasticsearch", "databases-for-enterprisedb", "databases-for-etcd", "databases-for-mongodb", "databases-for-mysql", "databases-for-postgresql", "databases-for-redis", "event-notifications", "hs-crypto", "iam-identity", "is", "logdna", "logdnaat", "messagehub", "messages-for-rabbitmq", "mqcloud", "secrets-manager", "sysdig-monitor", "sysdig-secure"], target_service_name) if attributes.region != null
+    ])
+    error_message = "Provide a valid target service name that supports region attribute."
+  }
+  validation {
+    condition = alltrue([
       for target_service_name, attributes in var.target_service_details : (
-        contains(["cloud-object-storage", "codeengine", "container-registry", "containers-kubernetes", "containers-kubernetes-cluster", "containers-kubernetes-management", "databases-for-cassandra", "databases-for-elasticsearch", "databases-for-enterprisedb", "databases-for-etcd", "databases-for-mongodb", "databases-for-mysql", "databases-for-postgresql", "databases-for-redis", "event-notifications", "hs-crypto", "iam-identity", "is", "logdna", "logdnaat", "messagehub", "messages-for-rabbitmq", "mqcloud", "secrets-manager", "sysdig-monitor", "sysdig-secure"], target_service_name) &&
+        contains(["container-registry"], target_service_name) &&
         (
           !(attributes.region != null && attributes.geography != null)
         )
       )
     ])
-    error_message = "Both `region` and `geography` cannot be set simultaneously for the target service."
+    error_message = "Both `region` and `geography` cannot be set simultaneously for the container registry service."
   }
 
   validation {
