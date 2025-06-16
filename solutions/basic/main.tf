8eda691dcdaf194c81114fe22a23a7590bd2845a
Dishank-Kalra1 <Dishank.Kalra1@ibm.com>
2025-06-16 23:10:55 +0530

    fix: cbr_zones variable definition

--- solutions/basic/main.tf
+++ solutions/basic/main.tf
@@ -6,114 +6,97 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 }
 
 ##############################################################################
-# CBR zone & rule creation
+# CBR zone creation
 ##############################################################################
 
-module "cbr_zone" {
-  for_each           = var.cbr_zones
-  source             = "../../modules/cbr-zone-module"
-  name               = var.prefix != null ? "${var.prefix}-${each.value.name}" : each.value.name
-  account_id         = each.value.account_id == null ? data.ibm_iam_account_settings.iam_account_settings.account_id : each.value.account_id
-  zone_description   = each.value.zone_description
-  addresses          = each.value.addresses
-  excluded_addresses = each.value.excluded_addresses
-}
-
 locals {
-  cbr_zone_ids = flatten([
-    for zone in module.cbr_zone : [
-      zone.zone_id
-    ]
-  ])
-
-  cbr_zone_rule_context = flatten([
-    for key, zone in var.cbr_zones : {
-      attributes = [
-        {
-          name  = "networkZoneId"
-          value = module.cbr_zone[key].zone_id
-        }
-      ]
-    } if zone.add_zone_to_rules
-  ])
+  prefix = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
 }
+
+module "cbr_zone" {
+  for_each              = var.cbr_zones
+  source                = "../../modules/cbr-zone-module"
+  name                  = "${local.prefix}${each.value.name}"
+  account_id            = each.value.account_id == null ? data.ibm_iam_account_settings.iam_account_settings.account_id : each.value.account_id
+  zone_description      = each.value.zone_description
+  addresses             = each.value.addresses
+  excluded_addresses    = each.value.excluded_addresses
+  use_existing_cbr_zone = each.value.use_existing_cbr_zone
+  existing_zone_id      = each.value.existing_zone_id
+}
+
+
+##############################################################################
+# CBR rule creation
+##############################################################################
+
+# locals {
+#   rule_zone_contexts = {
+#     for rule_key, rule_val in var.cbr_rules : rule_key => [
+#       for zone_key in rule_val.zone_keys : {
+#         attributes = [
+#           {
+#             name  = "networkZoneId"
+#             value = module.cbr_zone[zone_key].zone_id
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# }
+
 
 # module "cbr_rule" {
-#   depends_on       = [module.cbr_zone]
 #   for_each         = var.cbr_rules
-#   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module"
-#   version          = "1.19.0"
-#   rule_description = each.value.description
+#   source           = "../../modules/cbr-rule-module"
+#   rule_description = each.value.rule_description
 #   enforcement_mode = each.value.enforcement_mode
-#   rule_contexts    = concat(local.cbr_zone_rule_context, each.value.rule_contexts)
+#   rule_contexts    = concat(local.rule_zone_contexts[each.key], each.value.rule_contexts)
 #   resources        = each.value.resources
 #   operations       = each.value.operations
 # }
-locals {
-  rule_zone_contexts = {
-    for rule_key, rule_val in var.cbr_rules : rule_key => [
-      for zone_key in rule_val.zone_keys : {
-        attributes = [
-          {
-            name  = "networkZoneId"
-            value = module.cbr_zone[zone_key].zone_id
-          }
-        ]
-      }
-    ]
-  }
-}
 
 
-module "cbr_rule" {
-  for_each         = var.cbr_rules
-  source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module"
-  version          = "1.19.0"
-  rule_description = each.value.rule_description
-  enforcement_mode = each.value.enforcement_mode
-  rule_contexts    = concat(local.rule_zone_contexts[each.key], each.value.rule_contexts)
-  resources        = each.value.resources
-  operations       = each.value.operations
-}
+# locals {
+#   cbr_rule_ids = flatten([
+#     for rule in module.cbr_rule : [
+#       rule.rule_id
+#     ]
+#   ])
 
+#   cbr_rule_crns = flatten([
+#     for rule in module.cbr_rule : [
+#       rule.rule_crn
+#     ]
+#   ])
+# }
 
-locals {
-  cbr_rule_ids = flatten([
-    for rule in module.cbr_rule : [
-      rule.rule_id
-    ]
-  ])
+# ##############################################################################
+# # Access policy to view CBRs
+# ##############################################################################
 
-  cbr_rule_crns = flatten([
-    for rule in module.cbr_rule : [
-      rule.rule_crn
-    ]
-  ])
-}
+# data "ibm_iam_access_group" "service_id_access_group" {
+#   access_group_name = var.cabin_service_id_access_group
+# }
 
+# resource "ibm_iam_access_group_policy" "network_zone_access_policy" {
+#   for_each = module.cbr_zone
 
-data "ibm_iam_access_group" "service_id_access_group" {
-  access_group_name = var.cabin_service_id_access_group
-}
+#   access_group_id = data.ibm_iam_access_group.service_id_access_group.groups[0].id
+#   roles           = ["Administrator"]
 
-resource "ibm_iam_access_group_policy" "network_zone_access_policy" {
-  for_each = module.cbr_zone
+#   resource_attributes {
+#     name  = "resource"
+#     value = each.value.zone_id
+#   }
 
-  access_group_id = data.ibm_iam_access_group.service_id_access_group.groups[0].id
-  roles           = ["Administrator"]
+#   resource_attributes {
+#     name  = "resourceType"
+#     value = "zone"
+#   }
 
-  resource_attributes {
-    name  = "resource"
-    value = each.value.zone_id
-  }
-
-  resource_attributes {
-    name  = "resourceType"
-    value = "zone"
-  }
-
-  resource_attributes {
-    name  = "serviceName"
-    value = "context-based-restrictions"
-  }
-}
+#   resource_attributes {
+#     name  = "serviceName"
+#     value = "context-based-restrictions"
+#   }
+# }
--- solutions/basic/variables.tf
+++ solutions/basic/variables.tf
@@ -10,19 +10,40 @@ variable "ibmcloud_api_key" {
 
 variable "prefix" {
   type        = string
-  description = "Prefix to append to all resources created by this example"
   default     = null
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-cos. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
+
+  validation {
+    # - null and empty string is allowed
+    # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
+    # - Starts with a lowercase letter: [a-z]
+    # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
+    # - Must not end with a hyphen (-): [a-z0-9]
+    condition = (var.prefix == null || var.prefix == "" ? true :
+      alltrue([
+        can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
+        length(regexall("--", var.prefix)) == 0
+      ])
+    )
+    error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
+  }
+
+  validation {
+    # must not exceed 16 characters in length
+    condition     = length(var.prefix) <= 16
+    error_message = "Prefix must not exceed 16 characters."
+  }
 }
 
 ##############################################################################
 # Cabin IAM Access Group Name
 ##############################################################################
 
-variable "cabin_service_id_access_group" {
-  description = "The name of the service id access group for the cabin.  Format: <cabin-name>_service_id where <cabin-name> is from the cabin config 'cabin_data.name'"
-  type        = string
-  default     = null
-}
+# variable "cabin_service_id_access_group" {
+#   description = "The name of the service id access group for the cabin.  Format: <cabin-name>_service_id where <cabin-name> is from the cabin config 'cabin_data.name'"
+#   type        = string
+#   default     = null
+# }
 
 ##############################################################################
 # Zone Related Input Variable
@@ -64,12 +85,12 @@ variable "cbr_zones" {
   EOT
   type = map(object({
     # By default account id is populated using data block where the CBR is created, else it can be passed if required
-    account_id = optional(string)
+    account_id = optional(string, null)
     name       = string
     # By default, the description is null
-    zone_description = string
+    zone_description = optional(string, null)
     # By default, the addresses is empty list
-    addresses = list(object({
+    addresses = optional(list(object({
       type  = optional(string)
       value = optional(string)
       ref = optional(object({
@@ -79,17 +100,16 @@ variable "cbr_zones" {
         service_name     = optional(string)
         service_type     = optional(string)
       }))
-    }))
+    })), [])
     # By default, the excluded_addresses is empty list
-    excluded_addresses = list(object({
+    excluded_addresses = optional(list(object({
       type  = optional(string)
       value = optional(string)
-    }))
+    })), [])
     # To update an existing CBR zone using `existing_zone_id`. By default, it is false
-    use_existing_cbr_zone = optional(bool)
+    use_existing_cbr_zone = optional(bool, false)
     # By default, it is null
-    existing_zone_id = optional(string)
-
+    existing_zone_id = optional(string, null)
   }))
   default = {}
   # Validation happens in the rule module
@@ -99,72 +119,72 @@ variable "cbr_zones" {
 # Rule Related Input Variable
 ##############################################################################
 
-variable "cbr_rules" {
-  description = <<EOT
-  Map of CBR rules to be created. Each rule includes mapping to zone keys.
-  Example:
-  cbr_rules = {
-    "limit_cos_1" = {
-      name             = "Restrict COS Access"
-      description      = "Allow request to COS only from vpc_zone and ip_zone"
-      enforcement_mode = "enabled"
-      zone_keys        = ["vpc_zone", "zone_2"]
-      resources = [
-        {
-          service_name         = "cloud-object-storage"
-          resource_instance_id = "cos-instance-id-1"
-        }
-      ]
-    },
-    "limit_cos_1" = {
-      name             = "Restrict COS Access"
-      description      = "Allow request to COS from vpc_zone only"
-      enforcement_mode = "enabled"
-      zone_keys        = ["vpc_zone"]
-      resources = [
-        {
-          service_name         = "cloud-object-storage"
-          resource_instance_id = "cos-instance-id-2"
-        }
-      ]
-    }
-  }
-  EOT
-  type = map(object({
-    # By default, rule_description is null if not passed
-    rule_description = optional(string)
-    # By default, rule_contexts is empty list if not passed
-    rule_contexts = optional(list(object({
-      attributes = list(object({
-        name  = string
-        value = string
-      }))
-      # check if attributes has to be made optional because it is already optional in variable definiton
-    })))
-    # Valid values for enforcement mode can be 'enabled', 'disabled' and 'report'
-    enforcement_mode = string
-    resources = list(object({
-      attributes = optional(list(object({
-        name     = string
-        value    = string
-        operator = optional(string)
-      })))
-      tags = optional(list(object({
-        name     = string
-        value    = string
-        operator = optional(string)
-      })))
-    }))
-    # By default it will protect all of the service and platform APIs the target service supports,
-    # For more information: check the default value of operations variable in modules/cbr-rule-module/variables.tf
-    operations = optional(list(object({
-      api_types = list(object({
-        api_type_id = string
-      }))
-    })))
-    # references keys from var.cbr_zones.
-    zone_keys = list(string)
-  }))
-  default = {}
-  # Validation happens in the rule module
-}
+# variable "cbr_rules" {
+#   description = <<EOT
+#   Map of CBR rules to be created. Each rule includes mapping to zone keys.
+#   Example:
+#   cbr_rules = {
+#     "limit_cos_1" = {
+#       name             = "Restrict COS Access"
+#       description      = "Allow request to COS only from vpc_zone and ip_zone"
+#       enforcement_mode = "enabled"
+#       zone_keys        = ["vpc_zone", "zone_2"]
+#       resources = [
+#         {
+#           service_name         = "cloud-object-storage"
+#           resource_instance_id = "cos-instance-id-1"
+#         }
+#       ]
+#     },
+#     "limit_cos_1" = {
+#       name             = "Restrict COS Access"
+#       description      = "Allow request to COS from vpc_zone only"
+#       enforcement_mode = "enabled"
+#       zone_keys        = ["vpc_zone"]
+#       resources = [
+#         {
+#           service_name         = "cloud-object-storage"
+#           resource_instance_id = "cos-instance-id-2"
+#         }
+#       ]
+#     }
+#   }
+#   EOT
+#   type = map(object({
+#     # By default, rule_description is null if not passed
+#     rule_description = optional(string)
+#     # By default, rule_contexts is empty list if not passed
+#     rule_contexts = optional(list(object({
+#       attributes = list(object({
+#         name  = string
+#         value = string
+#       }))
+#       # check if attributes has to be made optional because it is already optional in variable definiton
+#     })))
+#     # Valid values for enforcement mode can be 'enabled', 'disabled' and 'report'
+#     enforcement_mode = string
+#     resources = list(object({
+#       attributes = optional(list(object({
+#         name     = string
+#         value    = string
+#         operator = optional(string)
+#       })))
+#       tags = optional(list(object({
+#         name     = string
+#         value    = string
+#         operator = optional(string)
+#       })))
+#     }))
+#     # By default it will protect all of the service and platform APIs the target service supports,
+#     # For more information: check the default value of operations variable in modules/cbr-rule-module/variables.tf
+#     operations = optional(list(object({
+#       api_types = list(object({
+#         api_type_id = string
+#       }))
+#     })))
+#     # references keys from var.cbr_zones.
+#     zone_keys = list(string)
+#   }))
+#   default = {}
+#   # Validation happens in the rule module
+# }
