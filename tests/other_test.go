0d71b1f5b8514365c8e591fe5aac60a58885e60d
mukul-palit <mukul.palit@ibm.com>
2026-04-09 15:58:44 +0530

    test

--- tests/other_test.go
+++ tests/other_test.go
@@ -1,18 +1,24 @@
 package test
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+)
+
 const multiResourceTerraformDir = "examples/multi-resource-rule"
 
-// temporary fix for pipeline to pass for IBM provider version bump
-// func TestRunMultiResourceExample(t *testing.T) {
-// 	t.Parallel()
+func TestRunMultiResourceExample(t *testing.T) {
+	t.Parallel()
 
-// 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-// 		Testing:      t,
-// 		TerraformDir: multiResourceTerraformDir,
-// 		Prefix:       "mrr-rule",
-// 	})
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:      t,
+		TerraformDir: multiResourceTerraformDir,
+		Prefix:       "mrr-rule",
+	})
 
-// 	output, err := options.RunTestConsistency()
-// 	assert.Nil(t, err, "This should not have errored")
-// 	assert.NotNil(t, output, "Expected some output")
-// }
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
